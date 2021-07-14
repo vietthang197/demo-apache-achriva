be8a5003aedde66894c70bf49e120ab6d79d0fd4
Lê Việt Thắng <levietthang1997@gmail.com>
2021-07-14 14:51:14 +0700

    Initial commit

---
+++ src/test/java/com/thanglv/demoapacheachriva/DemoApacheAchrivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanglv.demoapacheachriva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApacheAchrivaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
